f0e1824c00b04d7a17aca66355c713bce3ddc946
Richard Zhu <>
2022-02-22 17:03:35 +0000

    it begins

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+# This example requires the 'members' privileged intents
+
 import discord
-import json
+from discord.ext import commands
 import os
 import os.path
-from pathlib import Path
-from keep_alive import keep_alive
-from replit import db
-from discord.ext import commands
+import random
+token = os.environ['token']
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
 
+There are a number of utility commands being showcased here.'''
 
-bot = commands.Bot(command_prefix='$')
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
 
 @bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
-    
-keep_alive()
 
+
+bot.run(token)
