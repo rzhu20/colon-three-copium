3d12bc8efe68d2b51f7fd203b4e844c065b06592
Richard Zhu <88802255+rzhu20@users.noreply.github.com>
2022-02-24 02:19:09 +0000

    made help look a bit better

--- main.py
+++ main.py
@@ -24,6 +24,37 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix='!', description=description, intents=intents, case_insensitive=True)
 
+class MyHelp(commands.HelpCommand):
+  def get_command_signature(self, command):
+    return '%s%s %s' % (self.clean_prefix, command.qualified_name, command.signature)
+     
+  async def send_bot_help(self, mapping):
+    embed = discord.Embed(title="Help")
+    for cog, commands in mapping.items():
+      filtered = await self.filter_commands(commands, sort=True)
+      command_signatures = [self.get_command_signature(c) for c in filtered]
+      if command_signatures:
+        cog_name = getattr(cog, "qualified_name", "No Category")
+        embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
+    channel = self.get_destination()
+    await channel.send(embed=embed)
+
+  async def send_command_help(self, command):
+    embed = discord.Embed(title=self.get_command_signature(command))
+    embed.add_field(name='Help', value=command.help)
+    alias = command.aliases
+    if alias:
+      embed.add_field(name='Aliases', value=", ".join(alias), inline=False)
+    channel = self.get_destination()
+    await channel.send(embed=embed)
+
+  async def send_error_message(self, error):
+    embed = discord.Embed(title='Error', description=error)
+    channel = self.get_destination()
+    await channel.send(embed=embed)
+
+bot.help_command = MyHelp()
+
 class LeaderBoardPosition:
     def __init__(self, user, count):
         self.user = user
@@ -54,7 +85,7 @@ async def on_ready():
 #    db[k][c3_stats] = value
 #  await ctx.send('done transfering info')  
   
-@bot.command(brief='Leaderboard of :3', description='Leaderboard of :3 usages')
+@bot.command(brief='Leaderboard of :3', description='Leaderboard of :3 usages', help='Displays leaderboard of :3 usages')
 async def leaderboard(ctx):
   keys = db.keys()
   leaderboard_list = []
@@ -71,7 +102,7 @@ async def leaderboard(ctx):
     em.add_field(name=f"{str(i + 1)}. {name}", value=f"{value}", inline=False)
   await ctx.send(embed=em)
   
-@bot.command(brief='Show your :3 count', description='Show your :3 count')
+@bot.command(brief='Shows your :3 count', description='Shows your :3 count', help='Shows your :3 count')
 async def count(ctx):
   userID = ctx.author.id
   userID_string = '<@!' + str(userID) + '>'
@@ -94,7 +125,7 @@ async def count(ctx):
     bot_message = "colon three <:copium:856725125507186708>\n" + userID_string + " has used :3 " + str(user_total) + " times! Woman is that you?<:womanhappy:814749175031136266>"
   await ctx.send(bot_message)
 
-@bot.command(brief='Get Leaderboard of Average Wordle Scores', description='Get Leaderboard of Average Wordle Scores, Best SCore feature coming soon tm')
+@bot.command(brief='Get Leaderboard of Average Wordle Scores', description='Get Leaderboard of Average Wordle Scores, Best SCore feature coming soon tm', help='Displays leaderboard of average wordle scores')
 async def wordle(ctx):
   keys = db.keys()
   leaderboard_list = []
